092c14b0173bc9c347bc7871d6445dea72703f6c
Zeyang <linzeyang1997@yeah.net>
2018-11-14 23:58:51 +0800

    添加新功能操作函数

--- single_direction_linkedList.cpp
+++ single_direction_linkedList.cpp
@@ -10,7 +10,7 @@ struct node{
 	int data;
 };
 
-node* create(int data){
+struct node* create(int data){
 	node *node_head = (node*)malloc(sizeof(node));
 	node_head->next = NULL;
 	node_head->data = data;
@@ -67,7 +67,7 @@ void printlist(struct node * node_head){
 }
 
 
-node* find_pre(struct node* node_head, int pos){
+struct node* find_pre(struct node* node_head, int pos){
 	
 	struct node* node_tmp = (node*)malloc(sizeof(node));
 	node_tmp->data = -1;
@@ -91,7 +91,7 @@ node* find_pre(struct node* node_head, int pos){
 }
 	
 
-node* find_index(struct node* node_head, int pos){
+struct node* find_index(struct node* node_head, int pos){
 	
 	struct node* node_tmp = (node*)malloc(sizeof(node));
 	if (pos >= getlen(node_head)){
@@ -131,6 +131,7 @@ int put_index(struct node* &node_head, int pos, int data){
 }
 
  
+//attention please: the head node is labelled as one with pos = 0
 int delete_index(struct node* &node_head, int pos){
 	if (pos >=getlen(node_head)){
 		return -1;
@@ -146,37 +147,131 @@ int delete_index(struct node* &node_head, int pos){
 	}
 	struct node* node_del_pre = find_pre(node_head, pos);
 	int data_del = node_del_pre->next->data;
-	node_del_pre->next = node_del_pre->next->next;
+	node_del_pre->next = node_del_pre->next->next;	
+	//warning:The node pointed by node_del_pre->next previously has not been released before return.
 	return data_del;
 }
 
+//attention please: the tail node is labelled as one with rvspos = 0
+int delete_rvsindex(struct node* &node_head, int rvspos){
+	return delete_index(node_head, getlen(node_head)-1-rvspos);
+}
+
+struct node* rvslist(struct node* &node_head){
+	int list_len = getlen(node_head);
+	if (list_len == 1 || list_len == 0) return node_head;
+	struct node* node_new_head = find_index(node_head,list_len-1);
+	struct node* node_right;
+	struct node* node_left;	
+	while(list_len!=1){
+		node_right = find_index(node_head,list_len-1);
+		node_left  = find_pre(node_head,list_len-1);
+		node_right->next = node_left;
+		node_left->next = NULL;
+		list_len--;
+	}
+	node_head->next = NULL;
+	node_head = node_new_head;
+	return node_head;
+}
 
 
+struct node* find_mid_node(struct node* node_head){
+	int len = getlen(node_head);
+	if (len % 2 == 0){
+		struct node* node_err = (node*)malloc(sizeof(node));
+		node_err->data = -1;
+		return node_err;
+	}else{
+		return find_index(node_head,(len-1)/2);
+	}
+}
+
+struct node* quick_init(int data0, int data1, int data2, int data3, int data4){
+	struct node* node_head = create(data0);
+	put_back(node_head,data1);
+	put_back(node_head,data2);
+	put_back(node_head,data3);
+	put_back(node_head,data4);
+	return node_head;
+}
+
+//attention please: the node with biggest (int)data must be the head node; 
+//the tail node has the smallest (int)data.
+int find_next_node_index_in_ordered(struct node* node_head, int compared_data){
+	int index = 0;
+	struct node* node_tmp = node_head;
+	if (node_tmp->data <= compared_data) return 0;
+	while(node_tmp->next != NULL){
+		if ((node_tmp->data >= compared_data) && (node_tmp->next->data <= compared_data)){
+			index++;
+			return index;
+		}
+		index++;
+		node_tmp = node_tmp->next;
+	}
+	return getlen(node_head);
+}
+
+int is_asc(struct node* node_head){
+	struct node* node_tmp = node_head;
+	int count = 0;
+	while(node_tmp->data >= node_tmp->next->data){
+		node_tmp = node_tmp->next;
+		count++;
+		if (count == getlen(node_head)-1) return 1;
+	}
+	return -1;
+}
+
+int merge_ordered(struct node* &node_head1,struct node* &node_head2){
+	int ERROR_UNORDERED = -2;
+	int ERROR_LEN_INVAL = -1;
+	if (getlen(node_head1) == 1 || getlen(node_head2) == 1){
+		return ERROR_LEN_INVAL;
+	}
+	if (getlen(node_head1) == 0 || getlen(node_head2) == 0){
+		return ERROR_LEN_INVAL;
+	}
+	int need_rvs = 0;
+	if (is_asc(node_head1)== -1) {
+		rvslist(node_head1);
+		if (is_asc(node_head1) == -1) return ERROR_UNORDERED;
+		need_rvs = 1;
+	}
+		
+	struct node* node_tmp = node_head2;
+	while(node_tmp->next != NULL){
+		int pos = find_next_node_index_in_ordered(node_head1,node_tmp->data);
+		put_index(node_head1, pos, node_tmp->data);
+		node_tmp = node_tmp->next;
+	}
+	int pos = find_next_node_index_in_ordered(node_head1,node_tmp->data);
+	put_index(node_head1, pos, node_tmp->data);
+	if (need_rvs) rvslist(node_head1);
+	return getlen(node_head1);
+}
+		
+	
 
 
-
+	
+	
 int main(){
 	
-	struct node *node_head = create(100);
-	put_index(node_head, 1,1000);
-	put_index(node_head,2,3333);
-	put_index(node_head, 1,1000);
-	put_index(node_head,2,3333);
-	put_index(node_head,0,5151);
-	delete_index(node_head,0);
-	delete_index(node_head,4);
-
-	delete_index(node_head,1);
-	delete_index(node_head,1);
-	delete_index(node_head,1);
-	delete_index(node_head,1);
-	delete_index(node_head,0);
-	
-	printlist(node_head);
+	struct node* node_head1 = quick_init(100,200,300,400,500);
+	//printlist(node_head);
+	//rvslist(node_head1);
+	//printlist(node_head);
 	
 	
-	
-
+	struct node* node_head2 = quick_init(101,10,30,4020,5);
+	merge_ordered(node_head1,node_head2);	
+	printlist(node_head1);
+	//cout<<find_next_node_index_in_ordered(node_head,300)<<endl;
+	//cout<<find_next_node_index_in_ordered(node_head,100)<<endl;
+	//cout<<find_next_node_index_in_ordered(node_head,31)<<endl;
+	//cout<<find_next_node_index_in_ordered(node_head,2)<<endl;
 	return 0;
 }
 	
